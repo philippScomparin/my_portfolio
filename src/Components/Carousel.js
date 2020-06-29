3b8fa16be2d21ffed336ba419add80a75a8ef9d7
Scomparin Philipp (Student Com16) <philipp.scomparin@stud-inf.unibz.it>
2020-06-30 01:33:03 +0200

    carousel

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,36 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ImgComp from './ImgComp'
 import Programming from '../images/programming.jpg'
 import Pepper from '../images/pepper.jpg'
 import Laptop from '../images/laptop.jpg'
 
-function Carousel() {
-    return (
-        <div className="carousel">
-        <div className="sliderhow middle">
-            <div className="slides">
 
-            <input type="radio" name="r" id="r1" checked/>
-            <input type="radio" name="r" id="r2"/>
-            <input type="radio" name="r" id="r3"/>
-            <div className="slide s1">
-                <img className="img" src={Programming} alt="Programming"/>
-            </div>
-            <div className="slide s2">
-                <img className="img "src={Pepper} alt="Pepper"/>
-            </div>
-            <div className="slide s3">
-                <img className="img" src={Laptop} alt=""/>
-            </div>
+
+
+
+
+function Carousel() {
+
+
+    const images_index = [<ImgComp src={Programming} />, <ImgComp src={Pepper} />, <ImgComp src={Laptop} />]
+    const [x, setx] = useState(0)
+    const [firstSelected, setfirstSelected] = useState({backgroundColor: "#fff"})
+    const [secondSelected, setsecondSelected] = useState({backgroundColor: null})
+    const [thirdSelected, setthirdSelected] = useState({backgroundColor: null})
+    
+   
+
+
+    function changeImage(event) {
+        const selectedImage = event.target.getAttribute("name")
+        if(selectedImage === "pepper"){
+            setx(-100)
+        }
+        else if(selectedImage === "laptop"){
+            setx(-200)
+        }
+        else {
+            setx(0)
+        }
+
+        setLabelBackground(selectedImage)
+    }
+
+    function setLabelBackground(selectedImage){
+        if(selectedImage === "pepper"){
+            setsecondSelected({backgroundColor: "#fff"})
+            setfirstSelected({backgroundColor: null})
+            setthirdSelected({backgroundColor: null})
+        }
+        else if(selectedImage === "laptop"){
+            setsecondSelected({backgroundColor: null})
+            setfirstSelected({backgroundColor: null})
+            setthirdSelected({backgroundColor: "#fff"})
+        }
+        else {
+            setsecondSelected({backgroundColor: null})
+            setfirstSelected({backgroundColor: "#fff"})
+            setthirdSelected({backgroundColor: null})
+            
+        }
+
+    }
+
+    return (
+
+        
+        
+
+        <>
+            <div className="carousel">
+                {images_index.map((element, index) => {
+                    return (
+                        <div className="slide"
+                            key={index}
+                            style={{ transform: `translateX(${x}%)` }}>
+                            {element}
+                        </div>)
+                })}
+                <div className="navigation">
+                    <label className="imageIndex" name="programming" onClick={changeImage} style={firstSelected}></label>
+                    <label className="imageIndex" name="pepper" onClick={changeImage} style={secondSelected}></label>
+                    <label className="imageIndex" name="laptop" onClick={changeImage} style={thirdSelected}></label>
                 </div>
-            
-            <div className="navigation">
-                <label for="r1" className="bar"></label>
-                <label for="r2" className="bar"></label>
-                <label for="r3" className="bar"></label>
             </div>
-            
-        </div>
-        </div>
+
+        </>
     )
 
 }
---
+++ src/Components/ImgComp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ImgComp(props) {
+    return (
+        
+            <img src={props.src} alt="slideImage"/>
+        
+    )
+}
+
+export default ImgComp
